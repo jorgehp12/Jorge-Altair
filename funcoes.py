8651951eec297aafc33742b62bc5ca508eee28cc
Jorge Hungria e Prado <jorgehp@al.insper.edu.br>
2026-04-28 16:04:48 -0300

    ex. 1

--- funcoes.py
+++ funcoes.py
@@ -1,2 +1,11 @@
 # Jorge-Altair
-é um jogo no curso de desenvolvimento de software feito por Jorge Hungria e Altair Moises.
+import random
+
+def rolar_dados(n):
+    import random
+    lista_dado = []
+    for i in range(n):
+        d = random.randint(1, 6)
+        lista_dado.append(d)
+    return lista_dado
+
