f0d23f168440ef282ec182da73d9c2c574314a0b
Jorge Hungria e Prado <jorgehp@al.insper.edu.br>
2026-04-30 15:27:28 -0300

    ex. 7
    Add function to calculate points for high sequence.

--- funcoes.py
+++ funcoes.py
@@ -38,4 +38,10 @@ def calcula_pontos_sequencia_baixa(l):
     elif 3 in l and 4 in l and 5 in l and 6 in l:
         n += 15
     return n
-
+def calcula_pontos_sequencia_alta(l):
+    n = 0
+    if 1 in l and 2 in l and 3 in l and 4 in l and 5 in l:
+        n += 30
+    elif 2 in l and 3 in l and 4 in l and 5 in l and 6 in l:
+        n += 30
+    return n
